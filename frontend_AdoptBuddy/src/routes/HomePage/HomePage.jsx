4d8a8413634aeb0529f8059248533e1825a1f59b
ahrafi16 <anjumhossainrafi16@gmail.com>
2025-06-13 02:02:18 +0600

    made the error page and the favicon and animation in the banner

--- frontend_AdoptBuddy/src/routes/HomePage/HomePage.jsx
+++ frontend_AdoptBuddy/src/routes/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import { motion } from "framer-motion";
 import pet from '../../assets/banner.png';
 import Button from '../Button/Button';
 const HomePage = () => {
@@ -10,17 +11,24 @@ const HomePage = () => {
                 <FaTwitter className="hover:border rounded-full hover:bg-blue-600 p-0.5 hover:text-white" />
             </div>
             <div className='flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20 py-10 w-full'>
-                <div className='w-1/3 flex justify-center items-center'>
+                <motion.div
+                    className='w-1/3 flex justify-center items-center'
+                    initial={{ x: -200, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}>
                     <img className='' src={pet} alt="" />
-
-                </div>
-                <div className="flex flex-col gap-10 justify-center items-center text-center text-amber-950 w-2/3">
+                </motion.div>
+                <motion.div
+                    className="flex flex-col gap-10 justify-center items-center text-center text-amber-950 w-2/3"
+                    initial={{ x: 200, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}>
                     <h1 className="text-3xl md:text-5xl lg:text-7xl text-left w-full leading-tight">Rescue, Adopt, Love <br /> Connecting Pets with <br /> Their Forever Homes!</h1>
                     <div className="flex flex-col md:flex-row gap-4 mt-5 w-full">
                         <Button>Adopt Now</Button>
                         <Button>Learn More</Button>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
     );
