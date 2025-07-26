7897af1a2fd2191084c80343a02dcc055d46d1f2
ahrafi16 <anjumhossainrafi16@gmail.com>
2025-07-27 02:20:56 +0600

    Updated

--- resources/js/components/PetCard/PetCard.jsx
+++ resources/js/components/PetCard/PetCard.jsx
@@ -20,56 +20,6 @@ const PetCard = ({ pet }) => {
     };
 
     return (
-        // <div>
-        //     <div
-        //         onClick={handlecardClick}
-        //         className="bg-indigo-50 border border-indigo-100 rounded-3xl shadow-md p-4 mx-auto transition-transform duration-500 transform cursor-pointer hover:scale-105 hover:shadow-lg"
-        //     >
-        //         <div className="flex justify-between items-start mb-4">
-        //             <span className={`${pet?.status === 'Available' ? "text-green-600" : "text-red-600"}  bg-white text-sm font-medium px-3 py-1 rounded-full`}>
-        //                 {pet ? pet.status : "Not found."}
-        //             </span>
-        //             <button
-        //                 onClick={(e) => {
-        //                     e.stopPropagation();
-        //                     setLiked(!liked);
-        //                 }}
-        //                 className={`text-xl ${liked ? "text-red-500" : "text-gray-300"
-        //                     } cursor-pointer`}
-        //             >
-        //                 <FaHeart />
-        //             </button>
-        //         </div>
-
-        //         <div className="flex justify-center mb-4">
-        //             <img
-        //                 src={`http://localhost:8000/storage/${pet.images?.[0]?.image || 'default.jpg'}`}
-        //                 className="w-full h-64 object-cover rounded-xl"
-        //             />
-        //         </div>
-
-        //         <p className="text-sm text-green-600 font-medium">{ }</p>
-        //         <h3 className="text-lg font-semibold">{pet?.name}</h3>
-        //         <p className="text-md text-gray-700 mb-4">
-        //             Breed : {pet ? pet.breed : "Not found"} | Age : {pet ? pet.age : "Not found"}
-        //         </p>
-        //         <p className="text-md text-gray-700 mb-4">
-        //             Location : {pet ? pet.location : "Location not found"}
-        //         </p>
-
-        //         <button
-        //             onClick={(e) => {
-        //                 e.stopPropagation();
-        //                 document.getElementById("my_modal_5").showModal();
-        //             }}
-        //             className="w-full cursor-pointer bg-indigo-500 hover:bg-indigo-600 text-white py-2 rounded-2xl font-medium"
-        //         >
-        //             Apply to Adopt
-        //         </button>
-        //     </div>
-
-        // </div>
-
         // new card
         <div className="md:w-sm">
             <div
@@ -96,12 +46,12 @@ const PetCard = ({ pet }) => {
                     {/* Heart button */}
                     <button
                         onClick={(e) => {
+                            handleLikeClick()
                             e.stopPropagation();
                             setLiked(!liked);
                         }}
-                        className={`absolute top-4 right-4 text-2xl ${
-                            liked ? "text-red-500" : "text-white"
-                        } hover:scale-110 transition-transform`}
+                        className={`absolute top-4 right-4 text-2xl ${liked ? "text-red-500" : "text-white"
+                            } hover:scale-110 transition-transform`}
                     >
                         <FaHeart className="drop-shadow-lg" />
                     </button>
